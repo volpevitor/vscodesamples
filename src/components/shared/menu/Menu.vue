ae358002fab60866fc4ce7fe9f9af23f12ca7a40
volpevitor <vitorvolpe@yahoo.com.br>
2020-10-20 21:18:38 -0300

    importação

---
+++ src/components/shared/menu/Menu.vue
@@ -0,0 +1,36 @@
+<template>
+    <nav>
+      <ul>       
+          <li v-for="rota in rotas">
+            <router-link :to="rota.path ? rota.path : '/'">{{rota.titulo}}</router-link>
+          </li>
+      </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+ props: {
+        rotas: {
+            type: Array, 
+            required: true
+        }
+    }
+
+}
+</script>
+
+<style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity .3s
+  }
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0
+  }
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+import './directives/Transform';
+
+import { routes } from './routes';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes, 
+  mode: 'history'
+});
+
+Vue.use(VueResource);
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
